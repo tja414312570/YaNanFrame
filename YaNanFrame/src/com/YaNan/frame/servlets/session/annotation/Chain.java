b091a6d19c1716eda51b9ca95536543d83450049
tja414312570 <414312570@qq.com>
2019-05-09 11:32:42 +0800

    注释

--- YaNanFrame/src/com/YaNan/frame/servlets/session/annotation/Chain.java
+++ YaNanFrame/src/com/YaNan/frame/servlets/session/annotation/Chain.java
@@ -3,7 +3,16 @@ package com.YaNan.frame.servlets.session.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * 放行地址，通过此配置的地址不会经过验证
+ * @author yanan
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Chain {
+	/**
+	 * 需要放行的地址
+	 * @return
+	 */
 	String[] chain();
 }
